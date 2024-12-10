572a0c0230742d046d9fed843554ca680d62121d
Aleksandr.Bendrikov <Aleksandr.Bendrikov@norbit.ru>
2024-12-10 23:15:15 +0300

    Реализиация логики вывода и создания статей

--- Models/Article.cs
+++ Models/Article.cs
@@ -2,8 +2,10 @@
 {
     public class Article
     {
+        public int Id { get; set; }
         public string Title { get; set; }
         public int IdCategory { get; set; }
+        public int IdUser { get; set; }
         public string Text { get; set; }
         public string Tags { get; set; }
     }
--- wwwroot/scripts/ArticleList.js
+++ wwwroot/scripts/ArticleList.js
@@ -1,46 +1,104 @@
-const url = 'http://localhost:5002/article/all'; 
+const url = 'http://localhost:5002/article'; 
 
-fetch(url, {
-    method: 'POST',
-    headers: {
-        'Content-Type': 'application/json',
-    },
-    body: JSON.stringify({}) 
-})
-    .then(response => {
-        if (!response.ok) {
-            throw new Error('Ошибка в ответе от сервера');
-        }
-        return response.json(); 
+function getArticleList() {
+    fetch(`${url}/all`, {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({})
     })
-    .then(data => {
-        console.log('Полученные статьи:', data); 
+        .then(response => {
+            if (!response.ok) {
+                throw new Error('Ошибка в ответе от сервера');
+            }
+            return response.json();
+        })
+        .then(data => {
+            console.log('Полученные статьи:', data);
 
-        const divContainer = document.getElementsByClassName('article-list');
+            const divContainer = document.getElementsByClassName('article-list');
 
-        data.forEach(article => {
-            const div = document.createElement('div');
-            div.classList.add('article-item'); 
+            data.forEach(article => {
 
-            const title = document.createElement('h2');
-            title.classList.add('article-title');
-            title.textContent = article.title;  
+                const link = document.createElement('a');
+                link.href = `articlepage.html?id=${article.id}`;
 
-            const topic = document.createElement('p');
-            topic.classList.add('article-topic');
-            topic.textContent = `Тема: ${article.topic}`; 
+                const div = document.createElement('div');
+                div.classList.add('article-item');
 
-            const tags = document.createElement('p');
-            tags.classList.add('article-tags');
-            tags.textContent = `Теги: ${article.tags}`;  
+                const title = document.createElement('h2');
+                title.classList.add('article-title');
+                title.textContent = article.title;
 
-            div.appendChild(title);
-            div.appendChild(topic);
-            div.appendChild(tags);
+                const topic = document.createElement('p');
+                topic.classList.add('article-topic');
+                topic.textContent = `Тема: ${article.topic}`;
 
-            divContainer[0].appendChild(div);
+                const tags = document.createElement('p');
+                tags.classList.add('article-tags');
+                tags.textContent = `Теги: ${article.tags}`;
+
+                link.appendChild(div);
+                div.appendChild(title);
+                div.appendChild(topic);
+                div.appendChild(tags);
+
+                divContainer[0].appendChild(link);
+            });
+        })
+        .catch(error => {
+            console.error('Ошибка при запросе:', error);
         });
+}
+
+function getArticle(id) {
+    fetch(`${url}/${id}`, {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({
+            id: id
+        })
     })
-    .catch(error => {
-        console.error('Ошибка при запросе:', error); 
-    });
+        .then(response => {
+            if (!response.ok) {
+                throw new Error('Ошибка в ответе от сервера');
+            }
+            return response.json();
+        })
+        .then(data => {
+            console.log('Полученные статьи:', data);
+
+            const divContainer = document.getElementsByClassName('article-list');
+
+            data.forEach(article => {
+                const div = document.createElement('div');
+                div.classList.add('article-item');
+
+                const title = document.createElement('h2');
+                title.classList.add('article-title');
+                title.textContent = article.title;
+
+                const topic = document.createElement('p');
+                topic.classList.add('article-topic');
+                topic.textContent = `Тема: ${article.topic}`;
+
+                const tags = document.createElement('p');
+                tags.classList.add('article-tags');
+                tags.textContent = `Теги: ${article.tags}`;
+
+                div.appendChild(title);
+                div.appendChild(topic);
+                div.appendChild(tags);
+
+                divContainer[0].appendChild(div);
+            });
+        })
+        .catch(error => {
+            console.error('Ошибка при запросе:', error);
+        });
+}
+
+getArticleList();
